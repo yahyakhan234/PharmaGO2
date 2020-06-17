e200d255233651d0ac3a8dfe31570177fe9dcfa8
yahyakhan234 <yahyakhan234@users.noreply.github.com>
2020-06-17 22:59:06 +0500

    order_request_page made/ changes dont remember

--- app/src/main/java/com/fyp/order_request_page.java
+++ app/src/main/java/com/fyp/order_request_page.java
@@ -2,7 +2,9 @@ package com.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,10 +13,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.button.MaterialButton;
 
 public class order_request_page extends AppCompatActivity
         implements OnMapReadyCallback {
     private GoogleMap mMap;
+    public MaterialButton accept_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,22 @@ public class order_request_page extends AppCompatActivity
         setContentView(R.layout.activity_order_request_page);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        accept_button=findViewById(R.id.accept_order);
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.locCus);
         mapFragment.getMapAsync(this);
+        accept_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(order_request_page.this, pharmacy_price_order.class));
+                finish();
+            }
 
+        });
     }
 
     @Override
