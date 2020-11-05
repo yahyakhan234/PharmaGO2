f0a7da02aced3b682422186c1db7b990b08684b7
yahyakhan234 <yahyakhan234@users.noreply.github.com>
2020-11-06 00:16:00 +0500

    Lab Module complete

--- app/src/main/java/com/fyp/login_Screen.java
+++ app/src/main/java/com/fyp/login_Screen.java
@@ -1,34 +1,63 @@
 package com.fyp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.tiper.MaterialSpinner;
 
 public class login_Screen extends AppCompatActivity {
 
     MaterialSpinner spinner2;
     Button login_button,register1;
+
+    String email;
+    String password;
     private static final String[] ITEMS = {"Patient", "Pharmacy", "Laboratory"};
+    private FirebaseAuth mAuth;
 
     private ArrayAdapter<String> adapter;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+           protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onc check", "first on create");
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+
+
+            super.onCreate(savedInstanceState);
+
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mAuth = FirebaseAuth.getInstance();
+        Log.d("switch check", "check for firebase and reference");
+
 
 
         String selected_option;
@@ -36,6 +65,8 @@ public class login_Screen extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, ITEMS);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         final MaterialSpinner materialSpinner = findViewById(R.id.material_spinner);
+        final TextInputLayout em = findViewById(R.id.email1);
+        final TextInputLayout pw = findViewById(R.id.password1);
 
         materialSpinner.setAdapter(adapter);
         login_button=findViewById(R.id.login_button);
@@ -43,6 +74,7 @@ public class login_Screen extends AppCompatActivity {
         register1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 startActivity(new Intent(login_Screen.this, signup.class));
                 finish();
@@ -55,12 +87,36 @@ public class login_Screen extends AppCompatActivity {
             public void onClick(View v) {
 
 
+
         String selected_option= (String) materialSpinner.getSelectedItem();
+        String email= em.getEditText().getText().toString();
+        String password= pw.getEditText().getText().toString();
         Log.d("switch check", selected_option);
         switch (selected_option) {
             case "Patient": {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(login_Screen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(login_Screen.this,"Sucess",Toast.LENGTH_LONG).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startActivity(new Intent(login_Screen.this, dashboard.class));
 
-                        startActivity(new Intent(login_Screen.this, dashboard.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("chk", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(login_Screen.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+
+
+                            }
+                        });
+
                         finish();
                         break;
 
@@ -93,6 +149,9 @@ public class login_Screen extends AppCompatActivity {
 
 
 
+    }
+
+    private void updateUI(Object o) {
     }
 
 }
